4b78db9e246b64e0ce8c358ef39d98a40bc6c1c9
kwangncp21 <kw.nichapa@gmail.com>
2024-02-10 23:21:22 +0700

    correct controller file

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,20 +1,65 @@
-//route GET /api/v1/hospitals
-exports.getHospitals=(req,res,next)=>{
-    res.status(200).json({success:true,msg:'show all hospitalsTEST'});
+//route GET ALL /api/v1/hospitals
+const Hospital = require('../models/Hospital');
+
+exports.getHospitals= async(req,res,next)=>{
+    try{
+        const hospitals = await Hospital.find();
+        res.status(200).json({success:true,count:hospitals.length,data:hospitals});
+    }catch(err){
+        res.status(400).json({success:false});
+    }
 };
-//route GET /api/v1/hospitals/:id
-exports.getHospital=(req,res,next)=>{
-    res.status(200).json({success:true,msg:`show hospitals ${req.params.id}`});
+//route GET ONE /api/v1/hospitals/:id
+exports.getHospital= async(req,res,next)=>{
+    try{
+        const hospital = await Hospital.findById(req.params.id);
+
+        if(!hospital){
+            return res.status(400).json({success:false});
+        }
+        res.status(200).json({success:true,data:hospital});
+    }catch(err){
+        res.status(400).json({success:false});
+    }
+    
 };
 //route POST /api/v1/hospitals
-exports.createHospital=(req,res,next)=>{
-    res.status(200).json({success:true,msg:`Create new hospitals`});
+exports.createHospital= async(req,res,next)=>{
+    // console.log(req.body);
+    const hospital = await Hospital.create(req.body);
+    res.status(201).json({
+        success: true,
+        data: hospital
+    });
+    // res.status(200).json({success:true,msg:`Create new hospitals`});
 };
 //route PUT /api/v1/hospitals/:id
-exports.updateHospital=(req,res,next)=>{
-    res.status(200).json({success:true,msg:`Update hospitals ${req.params.id}`});
+exports.updateHospital= async(req,res,next)=>{
+    try{
+        const hospital = await Hospital.findByIdAndUpdate(req.params.id, req.body,{
+            new:true,
+            runValidators: true
+        });
+
+        if(!hospital){
+        return res.status(400).json({success:false});
+        }
+        res.status(200).json({success:true,data:hospital});
+
+    }catch(err){
+        res.status(400).json({success:false});
+    }
 };
 //route DELETE /api/v1/hospitals/:id
-exports.deleteHospital=(req,res,next)=>{
-    res.status(200).json({success:true,msg:`Deleteddddddd hospitals ${req.params.id}`});
+exports.deleteHospital= async(req,res,next)=>{
+    try{
+        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        if(!hospital){
+            return res.status(400).json({success:false})
+        }
+
+        res.status(200).json({success:true,data:{}});
+    }catch(err){
+        res.status(400).json({success:false});
+    }
 };
