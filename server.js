ba00a0469ca6df932156efcbd97b6de14a9819be
kwangncp21 <kw.nichapa@gmail.com>
2024-02-11 07:18:11 +0700

    add all file

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
+// Connect to database
+connectDB();
+
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 const hospitals = require(`./routes/hospitals`);
+// const { connect } = require('mongoose');
 app.use(`/api/v1/hospitals`,hospitals);
 
 const PORT=process.env.PORT||3000;
-app.listen(PORT, console.log('server running in',process.env.NODE_ENV,'mode on port',PORT))
+const server= app.listen(PORT, console.log('server running in',process.env.NODE_ENV,'mode on port',PORT));
+
+// Handle unhandle promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    // close server & exit process
+    server.close(()=>process.exit(1));
+});
